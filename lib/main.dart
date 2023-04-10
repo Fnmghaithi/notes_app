ba59178866386dff8f6f7dc3b86ab1bf58742ef0
Faisal <fnmghaithi@gmail.com>
2023-04-10 23:24:49 +0400

    add addNote Method to add note cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes_app/constants.dart';
+import 'package:notes_app/models/note.dart';
 import 'package:notes_app/views/notes_view.dart';
 
 void main() async {
   await Hive.initFlutter();
 
   await Hive.openBox(kNotesBox);
+
+  Hive.registerAdapter(NoteAdapter());
 
   runApp(const NotesApp());
 }
